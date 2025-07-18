879149bc43de575e6e7a08e804c55e18b5576efd
redDwarf03 <sylvain@archethic.net>
2025-07-18 21:52:15 +0200

    Just soon page...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
   import '../app.css';
-  import Navbar from '$lib/components/Navbar.svelte';
   import { page } from '$app/state';
-  import WalletProvider from '$lib/components/solana/WalletProvider.svelte';
-  import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
-  import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 
   let { children } = $props();
 
-  const localStorageKey = 'walletAdapter';
-  const walletAdapters = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
-
   const title = $derived.by(() => {
-    let pre = 'viralmind.ai';
+    let pre = 'CLONES';
     const route = page.route.id?.split('/')[1];
     if (route) {
       pre += ' - ' + route[0].toUpperCase() + route.slice(1, route.length);
@@ -24,27 +17,25 @@
 <svelte:head>
   <!-- HTML Meta Tags -->
   <title>{title}</title>
-  <meta name="description" content="The Next Meta in Agentic AI." />
+  <meta name="description" content="Train. Tokenize. Execute." />
 
   <!-- Facebook Meta Tags -->
   <meta property="og:url" content={page.url.toString()} />
   <meta property="og:type" content="website" />
   <meta property="og:title" content={title} />
-  <meta property="og:description" content="The Next Meta in Agentic AI." />
-  <meta property="og:image" content="https://viralmind.ai/favicon.png" />
+  <meta property="og:description" content="Train. Tokenize. Execute." />
+  <meta property="og:image" content="https://clones-ai.com/favicon.png" />
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:domain" content="viralmind.ai" />
+  <meta property="twitter:domain" content="clones-ai.com" />
   <meta property="twitter:url" content={page.url.toString()} />
   <meta name="twitter:title" content={title} />
-  <meta name="twitter:description" content="The Next Meta in Agentic AI." />
-  <meta name="twitter:image" content="https://viralmind.ai/favicon.png" />
+  <meta name="twitter:description" content="Train. Tokenize. Execute." />
+  <meta name="twitter:image" content="https://clones-ai.com/favicon.png" />
 </svelte:head>
 
-<WalletProvider {localStorageKey} wallets={walletAdapters} autoConnect />
-<div class="bg-white">
-  <Navbar />
+<div class="min-h-screen bg-black">
   {@render children()}
   <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-TQ5Z1BBEGG"></script>
